adb6b952628deee25dc41ff31ea3041e8160baf4
Andrea Michelotti <michelo@pldanteco109.lnf.infn.it>
2025-10-26 23:15:18 +0100

    added github workflow

--- infn_ophyd_hal/__init__.py
+++ infn_ophyd_hal/__init__.py
@@ -4,7 +4,6 @@ from .ophyd_ps import OphydPS,ophyd_ps_state,PowerSupplyFactory,PowerSupplyState
 from .ophyd_ps_sim import OphydPSSim
 from .ophyd_ps_dantemag import OphydPSDante
 from .unimag_ophyd_ps import OphydPSUnimag
-from .io_rtd import OphydRTD
 from .io_basic import OphydDI, OphydDO, OphydAI, OphydAO, OphydRTD
 
 PowerSupplyFactory.register_type("sim", OphydPSSim)
